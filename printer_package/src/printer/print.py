3e5bd64ad3dd20c1dbed4bf945214052684920bb
sirinerajhi <sirine.rajhi@student.vives.be>
2022-03-30 15:40:37 +0200

    finished printer basic functions

--- printer_package/src/printer/print.py
+++ printer_package/src/printer/print.py
@@ -1,14 +1,26 @@
 import time
 import serial
 
-ser = serial.Serial("/dev/ttyACM0", baudrate = 9600, parity=serial.PARITY_NONE, 
-stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
+class Printer:
+	#ser = serial.Serial("/dev/ttyACM0", baudrate = 9600, parity=serial.PARITY_NONE, 
+	#stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
+
+	def __init__(self, port):
+		self.__ser = serial.Serial('%s'%(port), baudrate = 9600, parity=serial.PARITY_NONE, 
+		stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
+		print("New printer created")
+
+	def print_text(self, text):
+		self.__ser.write(b'%s\n'%(text).encode())
+		time.sleep(1)
+		readedtext = self.__ser.readline()
+		print("Printed following text: \n")
+		print(readedtext)
 
 
-def print_text(text):
-	ser.write(b'%s\n'%(text).encode())
-	time.sleep(1)
-	readedtext = ser.readline()
-	print(readedtext)
+	def set_port(self, port):
+                self.__ser = serial.Serial('%s'%(port), baudrate = 9600, parity=serial.PARITY_NONE,
+                stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
+                print("Port updated to: %s" %(port))
 
 
